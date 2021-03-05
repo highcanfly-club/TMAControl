966d997cbe4e0da7c5f3edf749ad09632fbbd78e
Ronan LE MEILLAT <ronan.le_meillat@sctg.eu.org>
2021-03-05 10:04:11 +0100

    typo

--- app/Http/Controllers/TMAStateViewOnlyController.php
+++ app/Http/Controllers/TMAStateViewOnlyController.php
@@ -44,7 +44,7 @@ class TMAStateViewOnlyController extends Controller
     {
         $decoded_message = json_decode($request->message);
         $message_date = \DateTime::createFromFormat(\DateTime::ATOM, $decoded_message->timestamp);
-        $now = $dateTime = new \DateTime();
+        $now = new \DateTime();
         $message_date_diff = abs($now->getTimestamp() - $message_date->getTimestamp());
 
         if ( ($this->isSgnatureValid($request->message,$request->signature)) && ($message_date_diff < env("CRYPTO_MAX_SKEW_SECONDS")) )
